5ec19090823e1f630dbab0311e8ddf679a19e364
Iliya5digital <115736491+Iliya5digital@users.noreply.github.com>
2023-07-18 11:13:54 +0300

    Add files via upload

---
+++ local/php_interface/init.php
@@ -0,0 +1,10 @@
+<?php
+AddEventHandler("main", "OnEndBufferContent", "removeType");
+function removeType(&$content)
+{
+    $content = replace_output($content);
+}
+function replace_output($d)
+{
+    return str_replace(' type="text/javascript"', "", $d);
+}
